26975b2476a406ab912a05577c86f06d474f0477
Rafael <rafael.hernandez@espn.com>
2018-06-20 13:17:59 -0400

    reverting flagIcon usage for now; debugging crash

--- format.js
+++ format.js
@@ -1,10 +1,8 @@
 const _ = require('lodash');
-const { teams } = require('./config');
 
-const flagIcon = (matchData, country) => teams[_.get(matchData, country).toLowerCase()] && teams[_.get(matchData, country).toLowerCase()].flagIcon || '';
-const vs = matchData => `${flagIcon(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.country')} vs ${flagIcon(matchData, 'away_team.country')} ${_.get(matchData, 'away_team.country')}`;
+const vs = matchData => `${_.get(matchData, 'home_team.country')} vs ${_.get(matchData, 'away_team.country')}`;
 const vsTime = matchData => `${vs(matchData)} (${new Date(matchData.datetime)})`;
-const vsScore = matchData => `${flagIcon(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')} ${_.get(matchData, 'away_team.country')} ${flagIcon(matchData, 'away_team.country')}`;
+const vsScore = matchData => `${_.get(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')} ${_.get(matchData, 'away_team.country')}`;
 const score = matchData => `${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')}`;
 
 module.exports = {
