e5c1ba3ec4798b5c3c5f8ed618d7027c902152ff
Rafael <rafael.hernandez@espn.com>
2018-06-20 13:12:44 -0400

    fixing mapping with matchData and country in flagIcon function

--- format.js
+++ format.js
@@ -1,10 +1,10 @@
 const _ = require('lodash');
 const { teams } = require('./config');
 
-const flagIcon = (country) => teams[_.get(matchData, country).toLowerCase()] && teams[_.get(matchData, country).toLowerCase()].flagIcon || '';
-const vs = matchData => `${flagIcon('home_team.country')} ${_.get(matchData, 'home_team.country')} vs ${flagIcon('away_team.country')} ${_.get(matchData, 'away_team.country')}`;
+const flagIcon = (matchData, country) => teams[_.get(matchData, country).toLowerCase()] && teams[_.get(matchData, country).toLowerCase()].flagIcon || '';
+const vs = matchData => `${flagIcon(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.country')} vs ${flagIcon(matchData, 'away_team.country')} ${_.get(matchData, 'away_team.country')}`;
 const vsTime = matchData => `${vs(matchData)} (${new Date(matchData.datetime)})`;
-const vsScore = matchData => `${flagIcon('home_team.country')} ${_.get(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')} ${_.get(matchData, 'away_team.country')} ${flagIcon('away_team.country')}`;
+const vsScore = matchData => `${flagIcon(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.country')} ${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')} ${_.get(matchData, 'away_team.country')} ${flagIcon(matchData, 'away_team.country')}`;
 const score = matchData => `${_.get(matchData, 'home_team.goals')} : ${_.get(matchData, 'away_team.goals')}`;
 
 module.exports = {
